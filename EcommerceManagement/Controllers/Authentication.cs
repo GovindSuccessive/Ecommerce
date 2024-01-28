200d4ab6a85314ec8b3e42033b82267811e32114
govind.sharma <govind.sharma@GOVINDSHARMASS->
2024-01-28 23:45:37 +0530

    User Mangements

--- EcommerceManagement/Controllers/Authentication.cs
+++ EcommerceManagement/Controllers/Authentication.cs
@@ -8,9 +8,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EcommerceManagement.Controllers
 {
-    public class Authentication(SignInManager<UserModel> signInManager, UserManager<UserModel> userManager) : Controller
+    public class Authentication: Controller
     {
-       
+        private readonly SignInManager<UserModel> _signInManager;
+        private readonly UserManager<UserModel> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public Authentication(SignInManager<UserModel> signInManager, UserManager<UserModel> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+
         public IActionResult Login()
         {
             
@@ -23,7 +34,7 @@ namespace EcommerceManagement.Controllers
             if (ModelState.IsValid)
             {
                 //login
-                var result = await signInManager.PasswordSignInAsync(model.Username!, model.Password!, model.RememberMe, false);
+                var result = await _signInManager.PasswordSignInAsync(model.Username!, model.Password!, model.RememberMe, false);
 
                 if (result.Succeeded)
                 {
@@ -66,12 +77,16 @@ namespace EcommerceManagement.Controllers
                    
                 };
 
-                var result = await userManager.CreateAsync(user, model.Password!);
+                var result = await _userManager.CreateAsync(user, model.Password!);
 
                 if (result.Succeeded)
                 {
-                    await userManager.AddToRoleAsync(user, Roles.User.ToString());
-                    await signInManager.SignInAsync(user, false);
+                    if (_signInManager.IsSignedIn(User) && User.IsInRole("Admin"))
+                    {
+                        return RedirectToAction("GetAllUser", "Authentication");
+                    }
+                    await _userManager.AddToRoleAsync(user, Roles.User.ToString());
+                    await _signInManager.SignInAsync(user,isPersistent: false);
                     return RedirectToAction("Index", "Home");                   
                 }
                 foreach (var error in result.Errors)
@@ -82,9 +97,60 @@ namespace EcommerceManagement.Controllers
             return View(model);
         }
 
+        [HttpGet]
+        public IActionResult GetAllUser()
+        {
+            var users =_userManager.Users;
+            return View(users);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(string id, List<string> userRoles)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                ViewBag.ErroMessage = $"User with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var model = new UpdateUserModel()
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Address = user.Address,
+                PhoneNo = user.PhoneNo,
+                Password = user.Password,
+                Claims=userClaims.Select(c=>c.Value).ToList(),
+                Roles= userRoles
+            };
+
+
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> UpdateProduct(Guid id)
+        {
+
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteProduct(Guid id)
+        {
+
+        }
+
+
+        [HttpGet]
         public async Task<IActionResult> Logout()
         {
-            await signInManager.SignOutAsync();
+            await _signInManager.SignOutAsync();
             return RedirectToAction("Login");
         }
     }
---
+++ EcommerceManagement/Models/Dto/UpdateUserModel.cs
@@ -0,0 +1,35 @@
+﻿using EcommerceManagement.Constant;
+using Microsoft.Identity.Client;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceManagement.Models.Dto
+{
+    public class UpdateUserModel
+    {
+        public UpdateUserModel()
+        {
+            Claims = new List<string>();
+            Roles= new List<string>();
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNo { get; set; }
+
+        [Required][EmailAddress]
+        public string Email { get; set; }
+
+        [Required][PasswordPropertyText]
+        public string Password { get; set; }
+
+        public string Address { get; set; }
+
+        public List<string> Claims { get; set; }
+
+        public List<string> Roles {  get; set; }
+
+    }
+}
